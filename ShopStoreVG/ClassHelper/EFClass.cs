5c6c2da22621efd53c9a9836ad9cc73b0d0215d1
YukaChka <maxzerovol@gmail.com>
2023-02-02 12:27:22 +0300

    !@#
    123

---
+++ ShopStoreVG/ClassHelper/EFClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopStoreVG.DB;
+
+namespace ShopStoreVG.ClassHelper
+{
+    internal class EFClass
+    {
+        public static ShopStoreVGEntities1 Context { get; } = new ShopStoreVGEntities1();
+    }
+}
---
+++ ShopStoreVG/Pages/PublicPages/Login.xaml.cs
@@ -0,0 +1,46 @@
+﻿using ShopStoreVG.ClassHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static ShopStoreVG.ClassHelper.EFClass;
+
+namespace ShopStoreVG.Pages.PublicPages
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var auth = Context.User.ToList()
+                 .Where(i => i.Login == LoginInput.Text && i.Password == PSWInput.Password)
+                 .FirstOrDefault();
+            if (auth != null)
+            {
+                
+                MessageBox.Show("OK");
+            }
+            else
+            {
+                MessageBox.Show("Пользователь не найден", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
